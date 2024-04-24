311582a469d341e0f4afba783f5fc43d5309ba7e
Pedro-C4RVALHO <164107752+Pedro-C4RVALHO@users.noreply.github.com>
2024-04-23 21:19:57 -0300

    commit inicial

---
+++ Cliente.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Cliente {
+    String Nome;
+    double limite;
+
+    public static void main(String[] args) {
+        System.println("Digite o nome: " Nome);
+
+
+    }
+
+
+}
---
+++ Produto.java
@@ -0,0 +1,6 @@
+public class Produto {
+    String Mercadoria;
+    
+
+    
+}
